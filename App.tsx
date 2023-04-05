cc1f67c4827d671e62a9f4435d549e59c40ee530
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-05 19:41:56 -0300

    Styling the StatusBar

--- App.tsx
+++ App.tsx
@@ -4,12 +4,18 @@ import { ThemeProvider } from 'styled-components/native';
 
 import { Loading } from '@components/Loading';
 import theme from './src/theme';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
