564acc55600b3d5c77bd1a98bb53c3a3547582da
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-05 20:46:16 -0300

    Creating GroupCard Component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from '@components/Header';
 import Styles from './styles';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
     return (
@@ -10,6 +11,9 @@ export function Groups() {
                 title='Turmas'
                 subtitle="jogue com a sua turma"
             />
+
+            <GroupCard title='Turma do Curso' />
+            <GroupCard title='Turma da escola' />
         </Styles.Container>
     );
 }
