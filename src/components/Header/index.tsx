41b4a2f57ddb45e04cc7f211e486944dc895c5c5
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-05 20:31:11 -0300

    Creating Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+// import { useNavigation } from "@react-navigation/native";
+import logoImg from '@assets/logo.png';
+import Styles from "./styles";
+
+type Props = {
+    showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false }: Props) {
+
+    // const navigation = useNavigation()
+
+    function handleGoBack() {
+        // navigation.navigate('groups')
+    }
+
+    return (
+        <Styles.Container>
+            {
+                showBackButton &&
+                <Styles.BackButton onPress={handleGoBack}>
+                    <Styles.BackIcon />
+                </Styles.BackButton>
+            }
+            <Styles.Logo source={logoImg} />
+        </Styles.Container>
+    )
+}
