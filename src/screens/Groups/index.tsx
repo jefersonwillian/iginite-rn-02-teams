3f6da88055bf5fe2b7642851a2e58e05f9e107a4
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-11 18:58:25 -0300

    listing groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,13 @@
-import { Header } from '@components/Header';
-import Styles from './styles';
-import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import Styles from './styles';
 
 export function Groups() {
+    const [groups, setGroups] = useState<string[]>(['Turma 1', 'Turma 2', 'Turma 3']);
+    
     return (
         <Styles.Container>
             <Header />
@@ -12,8 +16,13 @@ export function Groups() {
                 subtitle="jogue com a sua turma"
             />
 
-            <GroupCard title='Turma do Curso' />
-            <GroupCard title='Turma da escola' />
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard title={item} />
+                )}
+            />
         </Styles.Container>
     );
 }
