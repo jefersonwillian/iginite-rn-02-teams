ddb161705294d1d4e4a40c480d867cad9b3eb5cb
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-12 20:58:41 -0300

    Creating the PlayerCard Component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,16 +1,20 @@
+import { FlatList } from 'react-native';
+import { useState } from "react";
+
+import Styles from "./styles";
+
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
-import { FlatList } from 'react-native';
-
+import { Button } from "@components/Button";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { Input } from "@components/Input";
-import { useState } from "react";
-import Styles from "./styles";
+import { ListEmpty } from "@components/ListEmpty";
+import { PlayerCard } from "@components/PlayerCard";
 
 export function Players() {
     const [team, setTeam] = useState('Time A')
-    const [players, setPlayers] = useState([])
+    const [players, setPlayers] = useState(['jeferson', 'sasa', 'kkk', 'oooo', 'oooos', 'ooooe', 'ooooeeeeee', 'oooosssssddd', 'oooo2222'])
     
     return (
         <Styles.Container>
@@ -52,6 +56,27 @@ export function Players() {
                     {players.length}
                 </Styles.NumberOfPlayers>
             </Styles.HeaderList>
+
+            <FlatList
+                data={players}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <PlayerCard
+                        name={item}
+                        onRemove={() => { }}
+                    />
+                )}
+                ListEmptyComponent={() => (
+                    <ListEmpty message="Não há pessoas nesse time" />
+                )}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={[{ paddingBottom: 100 }, !players.length && { flex: 1 }]}
+            />
+
+            <Button
+                title="Remover Turma"
+                type="SECONDARY"
+            />
         </Styles.Container>
     )
 }
