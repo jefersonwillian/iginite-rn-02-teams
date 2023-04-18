d432f9e580ce959e5d484e14b155246984b02872
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-18 18:52:40 -0300

    Utilizando a SafeAreaView

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,9 +1,10 @@
+import { SafeAreaView } from "react-native-safe-area-context";
 import styled from "styled-components/native";
 
-const Container = styled.SafeAreaView`
-    flex: 1;
-    background-color: ${({ theme }) => theme.COLORS.GRAY_600};
-    padding: 24px;
+const Container = styled(SafeAreaView)`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  padding: 24px;
 `;
 
 export default {
--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,6 +1,7 @@
 import styled, { css } from "styled-components/native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const Container = styled.View`
+const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   padding: 24px;
