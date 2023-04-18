656517ffdb2149ab20234efaa06b06f6cc212f71
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-18 20:10:22 -0300

    Local Storage - Abrindo turmas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -25,6 +25,10 @@ export function Groups() {
             console.log(error);
         }
     }
+
+    function handleOpenGroup(group: string) {
+        navigation.navigate('players', { group })
+    }
     
     useFocusEffect(useCallback(() => {
         fetchGroups()
@@ -42,7 +46,7 @@ export function Groups() {
                 data={groups}
                 keyExtractor={item => item}
                 renderItem={({ item }) => (
-                    <GroupCard title={item} />
+                    <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
                 )}
                 contentContainerStyle={!groups.length && { flex: 1 }}
                 ListEmptyComponent={() => (
