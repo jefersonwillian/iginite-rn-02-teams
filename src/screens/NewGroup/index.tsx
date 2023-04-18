8f44cc1bf84f0e3359b1666e90321e82f61ca67a
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-18 20:05:52 -0300

    Local Storage - Tratando exceções

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -12,6 +12,7 @@ import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
 
 export function NewGroup() {
     const [group, setGroup] = useState('');
@@ -20,10 +21,20 @@ export function NewGroup() {
 
     async function handleNew() {
         try {
+
+            if (group.trim().length === 0) {
+                return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+            }
+            
             await groupCreate(group);
             navigation.navigate('players', { group })
         } catch (error) {
-            console.log(error)
+            if (error instanceof AppError) {
+                Alert.alert('Novo Grupo', error.message);
+            } else {
+                Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+                console.log(error);
+            }
         }
     }
 
